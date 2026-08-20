a3fde0f672326a51a6ed81df9e3b3535793490cc
Wanjiku Kariuki <wanjiku7101@gmail.com>
2026-08-20 20:42:36 +0300

    created purchase database structure

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE purchase_receipts (
+    receipt_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    supplier TEXT NOT NULL,
+    purchase_date DATE NOT NULL,
+    total_amount REAL NOT NULL
+);
+
+CREATE TABLE purchase_items (
+    item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    receipt_id INTEGER NOT NULL,
+    item TEXT NOT NULL,
+    quantity_purchased INTEGER NOT NULL,
+    unit_cost REAL NOT NULL,
+    total_cost REAL NOT NULL,
+
+    FOREIGN KEY (receipt_id)
+        REFERENCES purchase_receipts(receipt_id)
+);
